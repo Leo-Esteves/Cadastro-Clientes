52b25c2d9fafebba74ddd5948e95dde09ca95d7b
Leo-Esteves <140625152+Leo-Esteves@users.noreply.github.com>
2025-03-24 22:08:00 -0300

    Add conexão com bando de dados

---
+++ CadastroClientes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data;
+
+namespace CLientes
+{
+    public class CadastroClientes
+    {
+        Conexao conexao = new Conexao();
+        SqlCommand cmd = new SqlCommand();
+        public string mensagem = "";
+
+        public CadastroClientes(string nome, string rg, string cpf, string endereco, string bairro, string cidade, string numero, string estado,string telefone, string email)
+        {
+            //Comandos Sql
+            cmd.CommandText = "Insert into Clientes (nome, rg, cpf, endereco, bairro, cidade, numero, estado, telefone, email) values (@nome, @rg, @cpf, @endereco, @bairro,@cidade, @numero, @estado, @telefone, @email)";
+
+            //Parametros
+            cmd.Parameters.AddWithValue("@nome",nome);
+            cmd.Parameters.AddWithValue("@rg", rg);
+            cmd.Parameters.AddWithValue("@cpf", cpf);
+            cmd.Parameters.AddWithValue("@endereco", endereco);
+            cmd.Parameters.AddWithValue("@bairro", bairro);
+            cmd.Parameters.AddWithValue("@cidade", cidade);
+            cmd.Parameters.AddWithValue("@numero", numero);
+            cmd.Parameters.AddWithValue("@estado", estado);
+            cmd.Parameters.AddWithValue("@telefone", telefone);
+            cmd.Parameters.AddWithValue("@email", email);
+
+            try
+            {
+                // COnectando ao bd -- class Conexao
+                cmd.Connection = conexao.conectar();
+
+                //Executar Comando
+                cmd.ExecuteNonQuery();
+
+                // desconectar bd
+                conexao.desconectar();
+
+                //Mostrar mensagem de erro ou sucesso -- Variavel declarada acima
+                this.mensagem = "Cadastrado com sucesso";
+
+            }
+            catch (SqlException e)// tratar um erro expessifico no sql, variavel "e" para guardar o erro
+            {
+                this.mensagem = "Erro ao se conectar com o banco de dados";
+            
+            }
+
+        }
+    }
+}
---
+++ Conexao.cs
@@ -0,0 +1,39 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+namespace CLientes
+{
+   public class Conexao
+    {
+        SqlConnection con = new SqlConnection();// instanciando a classe connection
+
+        // Construtor
+        public Conexao()
+        {
+            //passar a conexao com o bd
+            con.ConnectionString = @"Data Source=DESKTOP-KLC6HK7;Initial Catalog=ClientesErp;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+           
+        }
+        // metodo Conectar
+        public SqlConnection conectar()
+        {
+            // Verificação se o bd está aberto
+            if (con.State == System.Data.ConnectionState.Closed)
+            {
+                con.Open();
+            }
+               
+            return con;
+        }
+        // Metodo Desconectar
+        public void desconectar()
+        {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+
+           
+        }
+
+    }
+}
